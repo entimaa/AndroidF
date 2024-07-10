42ec7ceb2f9675f1005bb881e59443a3596dc90f
alaa abu rkeek <a.abur18@college.labs>
2024-07-10 16:34:55 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Register extends AppCompatActivity {
+
+    TextInputEditText editTextEmail,  editTextPassword;
+    Button buttonReg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonReg = findViewById(R.id.btn_register);
+    }
+}
